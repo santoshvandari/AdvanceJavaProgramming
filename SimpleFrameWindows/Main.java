a5339c87afd54aa386d4dd50eb293acf6ecc194e
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-06-02 15:09:36 +0545

    First Java Program Completed

---
+++ SimpleFrameWindows/Main.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+import javax.swing.*;
+class Main{
+    public static void main(String args[]){
+        JFrame frame = new JFrame("Test Frame");
+        frame.setSize(new Dimension(300,200));
+        frame.setLocation(100,100);
+        frame.setVisible(true);
+    }
+}
