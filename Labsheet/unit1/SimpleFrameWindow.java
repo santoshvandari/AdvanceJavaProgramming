9d9470af344efa7a4029b70e6219e4961c351944
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-06-07 17:39:15 +0545

    Labsheet Added

---
+++ Labsheet/unit1/SimpleFrameWindow.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+import javax.swing.*;
+class SimpleFrameWindow{
+    public static void main(String args[]){
+        JFrame jf = new JFrame("Santosh Bhandari");
+        jf.setSize(500,500);
+        jf.setVisible(true);
+    }
+}
