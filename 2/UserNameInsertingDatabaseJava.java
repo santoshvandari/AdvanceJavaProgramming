9287e04389cc63259138be30e67a227c68d4d8e2
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-06-11 13:52:01 +0545

    Database COnnection Code Written

---
+++ 2/UserNameInsertingDatabaseJava.java
@@ -0,0 +1,10 @@
+import java.sql.*;
+import javax.swing.*;
+
+
+
+class UserNameInsertingDatabaseJava {
+    public static void main(String args[]){
+        JFrame jf = new JFrame("Database Connection");
+    }
+}
