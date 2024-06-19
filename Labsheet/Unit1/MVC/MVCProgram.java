cab1b7df64b34c16d982c90f35ecd8a4ff9b44cb
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-06-19 07:06:01 +0545

    MVC

---
+++ Labsheet/Unit1/MVC/MVCProgram.java
@@ -0,0 +1,20 @@
+import java.awt.event.*;
+import javax.swing.*;
+
+class DataView{
+    DataView(){
+        JFrame jf = new JFrame("Student Information");
+        jf.setSize(500,500);
+        jf.setLayout(null);
+        jf.setVisible(true);
+    }
+}
+
+
+
+
+class MVCProgram{
+    public static void main(String args[]){
+
+    }
+}
