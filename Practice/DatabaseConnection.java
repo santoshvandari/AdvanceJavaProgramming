ed8964877ddabac6919989172855b1926711f352
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-08-23 15:51:32 +0545

    Updated

--- Practice/DatabaseConnection.java
+++ Practice/DatabaseConnection.java
@@ -3,24 +3,23 @@ import java.sql.*;
 class DatabaseConnection{
     public static void main(String[] args) {
         try{
-
             Class.forName("com.mysql.jdbc.Driver");
             String db="jdbc:mysql://localhost:3306/bca";
             Connection con = DriverManager.getConnection(db, "root", "");
-            if(con!=null){
-                Statement smt=con.createStatement();
-                System.out.println("DataBase Connected Successfully");
-                String sql="CREATE TABLE IF NOT EXISTS Student(id int, name varchar(30));";
-                smt.execute(sql);
-                System.out.println("Table Create Successfully");
-            }else{
-                System.out.println("Data Connection Failed");
-            }
+            Statement smt=con.createStatement();
+            System.out.println("DataBase Connected Successfully");
+            String sql="CREATE TABLE IF NOT EXISTS Student(id int, name varchar(30));";
+            smt.execute(sql);
+            System.out.println("Table Create Successfully");
+            // Inserting
+            String insert="INSERT INTO Student VALUES(1,'Santosh Bhandari'),(2,'Adi'),(3,'Krish')";
+            smt.execute(insert);
+            System.out.println("Data Insert Successfully");
+
+            // Reading 
+
         }catch(Exception ex){
             System.out.println(ex);
         }
-
-        String 
-
     }
 }
