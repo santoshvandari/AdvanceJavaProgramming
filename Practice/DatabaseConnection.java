147900a55cb6daf33ba4e000c3319097af04aa2c
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-08-23 16:00:47 +0545

    Updated

--- Practice/DatabaseConnection.java
+++ Practice/DatabaseConnection.java
@@ -12,11 +12,29 @@ class DatabaseConnection{
             smt.execute(sql);
             System.out.println("Table Create Successfully");
             // Inserting
-            String insert="INSERT INTO Student VALUES(1,'Santosh Bhandari'),(2,'Adi'),(3,'Krish')";
-            smt.execute(insert);
-            System.out.println("Data Insert Successfully");
+            // String insert="INSERT INTO Student VALUES(1,'Santosh Bhandari'),(2,'Adi'),(3,'Krish')";
+            // smt.execute(insert);
+            // System.out.println("Data Insert Successfully");
+
+
+            String insert="INSERT INTO Student VALUES(?,?)";
+            PreparedStatement pmt = con.prepareStatement(insert);
+            pmt.setInt(1,10);
+            pmt.setString(2, "Santosh Bhandari");
+            pmt.execute();
+
+
+
 
             // Reading 
+            String read = "SELECT * FROM Student;";
+            // PreparedStatement pmt = con.prepareStatement(read);
+            // ResultSet rs=pmt.executeQuery(read);
+            ResultSet rs = smt.executeQuery(read);
+            System.out.println("ID\tName");
+            while(rs.next()){
+                System.out.println(rs.getInt(1)+"\t"+rs.getString(2));
+            }
 
         }catch(Exception ex){
             System.out.println(ex);
